3f10ec8810292dddbf6f4bbb2ae79614865db8ca
David Lac <kiddmit3@gmail.com>
2017-08-14 20:01:49 -0700

    working

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key:'i5GZmE22m9camK7fBg12rN68Z',
+  consumer_secret:'FJnhftJHHBbxm6sgQoCQ0TucGJkBMjzd4BE80p7XdMXXBuFEMo',
+  access_token_key:'3240998394-gZhknlt39GV3WrIpwsABvminL1hMQlEkwyXKdYC',
+  access_token_secret:'eod3iIQ7aREz2IkYKA0yArV0CYyQsb9Cg5hArdmlvDw3i',
+}
+
+exports.spotifyKeys = {
+	id:'464311f7a3b843d294a6ed93b99881f4',
+	secret:'f98ff8c6ca3d48fba83b948bc529771f',
+}
+
+exports.omdbKeys = {
+	API_key:'40e9cece',
+}
